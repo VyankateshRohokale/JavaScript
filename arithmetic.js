aba4290d5eb598b1dfc50625957660cd405facc0
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-09-03 09:59:08 -0700

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,26 @@
+var arithmetic = /** @class */ (function () {
+    function arithmetic(A, B) {
+        this.no1 = A;
+        this.no2 = B;
+    }
+    arithmetic.prototype.Addition = function () {
+        var ans;
+        ans = this.no1 + this.no2;
+        return ans;
+    };
+    arithmetic.prototype.Substraction = function () {
+        var ans;
+        ans = this.no1 - this.no2;
+        return ans;
+    };
+    return arithmetic;
+}());
+var obj1 = new arithmetic(11, 10);
+var obj2 = new arithmetic(25, 21);
+console.log(obj1.no1);
+console.log(obj1.no2);
+var result = 0;
+result = obj1.Addition();
+console.log(result);
+result = obj2.Substraction();
+console.log(result);
