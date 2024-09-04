1e59ccdf9cf0b25a2f3b6b56f5b88ca90308eecf
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-09-04 08:14:58 -0700

    Add files via upload

---
+++ array4.js
@@ -0,0 +1,2 @@
+var marks = [11, 21, 51, 101, 111];
+console.log("number of elements in the array are : " + marks.length);
---
+++ array5.js
@@ -0,0 +1,7 @@
+var marks = [11, 21, 51, 101, 111];
+console.log("number of elements in the array are : " + marks.length);
+var icnt = 0;
+var i = 0;
+for (icnt = 0; icnt <= marks.length; icnt++) {
+    console.log(marks[icnt]);
+}
