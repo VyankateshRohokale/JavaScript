5b88cfbd57d0479af555e9c50b19fc7dacd75042
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-09-04 20:46:34 +0530

    Delete array5.js

--- array5.js
+++
@@ -1,7 +0,0 @@
-var marks = [11, 21, 51, 101, 111];
-console.log("number of elements in the array are : " + marks.length);
-var icnt = 0;
-var i = 0;
-for (icnt = 0; icnt <= marks.length; icnt++) {
-    console.log(marks[icnt]);
-}
